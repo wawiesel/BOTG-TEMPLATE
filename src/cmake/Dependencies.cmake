3b436fd6c3e1f8afbb877f38368548e966198a3f
William A. Wieselquist <ww5@ornl.gov>
2017-04-11 18:18:05 -0400

    [fixed] from BOTG

--- src/cmake/Dependencies.cmake
+++ src/cmake/Dependencies.cmake
@@ -1,18 +1,6 @@
-##
-##
-##src/cmake/Dependencies.cmake
-##------------------------------------------------------------------------------
-##Every TriBITS package must declare its dependencies.
-##If you are using BootsOnTheGround, then you can add TPLs
-##**after** ``TRIBITS_PACKAGE_DEFINE_DEPENDENCIES``.
-##
-##.. code-block:: cmake
-##
-
-#Depend on Testing123 only for testing, but have TPL Spdlog
-#available everywhere.
-TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-  TEST_REQUIRED_PACKAGES
-    Testing123
+botgPackageDependencies(
+    LIB_REQUIRED_PACKAGES
+        BootsOnTheGround_SPDLOG
+    TEST_REQUIRED_PACKAGES
+        Testing123
 )
-botgAddTPL( LIB REQUIRED SPDLOG )
